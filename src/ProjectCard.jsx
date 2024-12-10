dced1191a3ea69b0d26938f1f023a7069b76fe17
Bakhtovar Daleri <bakhtovarrahmatov@mail.ru>
2024-12-10 11:52:26 +0300

    Adaptive

--- src/ProjectCard.jsx
+++ src/ProjectCard.jsx
@@ -1,13 +1,17 @@
 import React from "react";
-import project1Img from "../../trips-gallery/src/assets/project1.jpg";
 
 function ProjectCard(props) {
+  const cardStyle = {
+    backgroundImage: `url(${props.image})`,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+  };
   return (
-    <div className="background">
-      <p>{props.text}</p>
+    <div style={cardStyle} className="project-card">
+      <p className="corner-label">{props.text}</p>
       <div>{props.isNew}</div>
-      <img src={props.image} alt={props.title} />
-      <p>
+      <p className="trip-info">
         {props.city}, {props.month} - {props.cost}
       </p>
     </div>
--- src/Projects.jsx
+++ src/Projects.jsx
@@ -6,42 +6,58 @@ import project2Img from "../../trips-gallery/src/assets/project2.jpg";
 import project3Img from "../../trips-gallery/src/assets/project3.jpg";
 import project4Img from "../../trips-gallery/src/assets/project4.jpg";
 
+const projects = [
+  {
+    city: "Тбилиси",
+    month: "апрель",
+    cost: "83.000",
+    image: project1Img,
+    text: "НА АВТОБУСЕ",
+    isNew: true,
+  },
+  {
+    city: "Стамбул",
+    month: "март",
+    cost: "110.000",
+    image: project2Img,
+    text: "НА САМОЛЕТЕ",
+    isNew: false,
+  },
+  {
+    city: "Дубай",
+    month: "июнь",
+    cost: "220.000",
+    image: project3Img,
+    text: "НА САМОЛЕТЕ",
+    isNew: true,
+  },
+  {
+    city: "Пхукет",
+    month: "сентябрь",
+    cost: "135.000",
+    image: project4Img,
+    text: "САМОЛЕТ + ПАРОМ",
+    isNew: true,
+  },
+];
+
 function Projects() {
   return (
     <div>
       <Heading level="h1" text="Галерея путешествий" />
-      <ProjectCard
-        city="Тбилиси"
-        month="апрель"
-        cost="83.000"
-        image={project1Img}
-        text="НА АВТОБУСЕ"
-        isNew={true}
-      />
-      <ProjectCard
-        city="Стамбул"
-        month="март"
-        cost="110.000"
-        image={project2Img}
-        text="НА САМОЛЕТЕ"
-        isNew={false}
-      />
-      <ProjectCard
-        city="Дубай"
-        month="июнь"
-        cost="220.000"
-        image={project3Img}
-        text="НА САМОЛЕТЕ"
-        isNew={true}
-      />
-      <ProjectCard
-        city="Пхукет"
-        month="сентябрь"
-        cost="135.000"
-        image={project4Img}
-        text="САМОЛЕТ + ПАРОМ"
-        isNew={true}
-      />
+
+      {projects.map(function (project) {
+        return (
+          <ProjectCard
+            city={project.city}
+            month={project.month}
+            cost={project.cost}
+            image={project.image}
+            text={project.text}
+            isNew={project.isNew}
+          />
+        );
+      })}
     </div>
   );
 }
